ba1c5c0b0aa30cb55619737a16ddcb5cdf244e7c
Gary Venus <garyjdougherty@gmail.com>
2025-11-27 21:20:24 -0500

    fixed DFetch

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getUserArtById, getUserById } from "../../fetches/UserFetches.jsx";
-import { DeleteFetch } from "../../fetches/Deletefetch.jsx";
+import { DeleteFetch } from "../../fetches/DeleteFetch.jsx";
 import { updateUserBio, updateUserEmail } from "../../fetches/userProfileFetches.jsx";
 import { updateUserName } from "../../fetches/userProfileFetches.jsx";
 import "./UserProfile.css";
